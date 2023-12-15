74943c01c2ee5fb38419c43e0f3b8b1f83ddcd81
Dou2ble <22otro@stockholmscience.se>
2023-12-15 14:28:32 +0100

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,46 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Silkscreen } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const silk = Silkscreen({ subsets: ['latin'], weight: '400' })
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={` flex flex-col min-h-screen bg-[#282828] text-white overflow-hidden ${silk.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <h1 className='text-4xl' ></h1>
+
+      <div className='group absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-3/4'>
+        <img src="/images/pancakeLarge.png" alt="Large Pancake" className='rounded-full transition-transform scale-[2] group-hover:scale-[2.3]'/>
+        <div className="font-Silkscreen absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-7xl font-bold text-center transition-transform group-hover:rotate-6 group-hover:scale-[1.2]">
+          Pancake Clicker
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='m-16'>
+        <div className='mt-[760px]'>
+          <p className='text-center text-xl'>A new kind of clicker game</p>
+          <p>Introducing "Pancake Clicker" – a new kind of clicker game that sizzles with excitement! Immerse yourself in the delectable world of flapjacks and unleash your inner chef as you embark on a mouth-watering clicking adventure. This extraordinary clicker experience goes beyond the ordinary, offering a syrupy blend of addictive tapping, golden griddle upgrades, and pancake perfection. Elevate your pancake prowess as you flip, stack, and click your way to pancake paradise. With buttery-smooth graphics and a batterific soundtrack, "Pancake Clicker" serves up a tantalizing treat for your gaming cravings. Are you ready to taste the sweet success of becoming the ultimate pancake maestro? Click your way to pancake perfection – it's not just a game; it's a flipping fantastic culinary journey!</p>
+          <br />
+        </div>
+        <div>
+          <p className='text-center text-xl'>explore different upgrades</p>
+          <p>Dive into the mouthwatering universe of "Pancake Clicker," where a symphony of delectable upgrades awaits your culinary prowess! Elevate your pancake empire with sizzling enhancements that will leave you craving for more. Unleash the "Golden Griddle" upgrade to accelerate your flipping speed, turning your pancake preparation into a mesmerizing culinary ballet. Upgrade your spatula to the "Epic Flipper" for a flawless flip every time, ensuring the perfect golden brown on every pancake. Harness the power of the "Butter Booster" to add a rich and creamy touch, making each click a taste bud-tantalizing experience. As you ascend the syrupy ranks, unlock the "Maple Mastery" to double your tapping efficiency, and watch the pancake pile grow exponentially. With an array of upgrades at your fingertips, "Pancake Clicker" isn't just a game—it's a gourmet adventure where every click takes you one step closer to pancake perfection!</p>
+          <br />
+        </div>
+        <div>
+          <p className='text-center text-xl'>intriguing features</p>
+          <p>Embark on a culinary odyssey with "Pancake Clicker," where every click unfolds a world of intriguing features that will whisk you away to pancake paradise! Delight in the immersive "Pancake Planet" mode, where your clicks resonate across a pancake-filled galaxy, unlocking cosmic upgrades and secret recipes. Unleash the "Time-Flipping Warp" feature, allowing you to bend time and cook pancakes at an otherworldly speed. Engage in epic "Pancake Challenges" against other players globally, testing your flipping finesse and strategic tapping skills. Discover the mystical "Flavor Fusion" laboratory, where you can concoct unique pancake blends, each with its own special boost. With customizable pancake skins, daily quests, and surprise events, "Pancake Clicker" keeps the excitement sizzling. Immerse yourself in a world where every click is a step towards unlocking the extraordinary—because flipping pancakes has never been this captivating! Get ready for a pancake adventure that transcends the batter.</p>
+          <br />
+        </div>
+        <div>
+          <p className='text-center text-xl'>satasfying gameplay</p>
+          <p>Indulge your senses in the deeply satisfying gameplay of "Pancake Clicker," where every click is a symphony of culinary delight! Experience the unparalleled pleasure of a perfectly timed flip with our intuitive tapping system, providing a tactile and gratifying sensation as you master the art of pancake preparation. Revel in the visually stunning animations as your pancake pile grows with each precision click, creating a mesmerizing feast for the eyes. The game's seamless progression and rewarding upgrades ensure a constant flow of satisfaction, enticing you to click your way to pancake perfection. With a user-friendly interface and an array of delightful sound effects, "Pancake Clicker" transforms the simple act of clicking into a deeply gratifying, sensory-rich experience. Are you ready to immerse yourself in a world where each click leaves you craving for more? Welcome to a gameplay experience that's as satisfying as a stack of freshly flipped pancakes!</p>
+        </div>
       </div>
+      
+      
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
